2435bdcdaf81e37c308b7a6c1c3f1bbf1a2231ea
pradeepy91 <y.pradeep.91@gmail.com>
2018-10-06 14:04:20 +0530

    Initial DB changes and table Creation check

---
+++ src/main/java/com/xmas/game/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.xmas.game.repository;
+
+
+import org.springframework.data.repository.Repository;
+
+import com.xmas.game.entity.User;
+
+public interface UserRepository extends Repository<User, Long> {
+
+}
