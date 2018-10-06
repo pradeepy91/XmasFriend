9a01fd38dbc25f6265f080263dbd64dd1e38f27a
pradeepy91 <y.pradeep.91@gmail.com>
2018-10-06 13:06:45 +0530

    First Commit

---
+++ src/main/java/com/xmas/game/XmasfriendApplication.java
@@ -0,0 +1,12 @@
+package com.xmas.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmasfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmasfriendApplication.class, args);
+	}
+}
